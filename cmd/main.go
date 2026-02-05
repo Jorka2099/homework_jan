3fa633e01035469b61b23e3decd50ecae919b7cf
George <kharazov.george@gmail.com>
2026-02-05 14:51:24 +0300

    <здесь имя вашего коммита, например first commit>

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Jorka2099/homework_jan/internal/server"
+)
+
+// main создает сервер и слушает порт
+func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	srv := server.MakeServer(logger)
+
+	if err := srv.Server.ListenAndServe(); err != nil {
+		logger.Fatal("Failed to start server ", err)
+	}
 }
